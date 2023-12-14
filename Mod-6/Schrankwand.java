cc1d32dfe2735266a5a29bb0865bd3fb90f52fa7
Tim Gabrikowski <tim@gabrikowski.de>
2023-12-14 14:22:25 +0100

    6.2 done

--- Mod-6/Schrankwand.java
+++ Mod-6/Schrankwand.java
@@ -3,40 +3,36 @@ import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
 
 public class Schrankwand extends Furniture
 {
-    private Schrank s1;
-    private Schrank s2;
-    private Schrank s3;
+    private ArrayList<Schrank> boards;
     
-    public Schrankwand (int x, int y, int o) {
+    public Schrankwand (int x, int y, int o, int a) {
         super(x, y, o);
         
-        s1 = new Schrank(x, y, o);
-        s2 = new Schrank(x, y, o);
-        s3 = new Schrank(x, y, o);   
+        boards = new ArrayList<Schrank>();
         
-        tiefe = s1.getDepth();
-        breite = s1.getWidth()*3;
+        for(int i = 0; i < a; ++i) {
+            boards.add(new Schrank(x, y, o));
+        }
+        
+        tiefe = boards.get(0).getDepth();
+        breite = boards.get(0).getWidth() * a;
         zeige();
     }
     public GeneralPath gibAktuelleFigur()
     {
         GeneralPath schrankwand = new GeneralPath();
         
-        AffineTransform t1 = new AffineTransform();
-        t1.translate(0, 0);
-        schrankwand.append(t1.createTransformedShape( s1.gibAktuelleFigur() ), false);
-        
-        AffineTransform t2 = new AffineTransform();
-        t2.translate(s2.getWidth(), 0);
-        schrankwand.append(t2.createTransformedShape( s2.gibAktuelleFigur() ), false);
-        
-        AffineTransform t3 = new AffineTransform();
-        t3.translate(s2.getWidth()*2, 0);
-        schrankwand.append(t3.createTransformedShape( s3.gibAktuelleFigur() ), false);
-        
+        int i = 0;
+        for(Schrank s : boards) {
+            AffineTransform t = new AffineTransform();
+            t.translate(s.getWidth() * i++, 0);
+            schrankwand.append(t.createTransformedShape( s.gibAktuelleFigur() ), false);
+        }
+    
         return schrankwand;
     }
 }
