d23649eb7215568b697ef1e0794ff20a540f8b78
Tim Gabrikowski <tim@gabrikowski.de>
2023-12-19 20:52:27 +0100

    animation groundwork

--- animation_6.4/src/scenes/example.tsx
+++ animation_6.4/src/scenes/example.tsx
@@ -1,8 +1,19 @@
 import {makeScene2D} from '@motion-canvas/2d';
-import {waitFor} from '@motion-canvas/core';
+import {waitFor, waitUntil, createSignal} from '@motion-canvas/core';
+import {Circle, Txt, Line} from '@motion-canvas/2d/lib/components';
 
 export default makeScene2D(function* (view) {
   // Create your animations here
 
-  yield* waitFor(5);
+  let opacity = createSignal(0);
+
+  view.add(
+    <>
+      <Txt text={"Java Adventures"} y={-100} fontSize={175} fill={"d62828"} fontFamily={'Ubuntu'} opacity={opacity}></Txt>
+      <Txt text={"Arrays, ArrayLists, Iterators"} y={150} fontSize={80} fill={"FCBF49"} fontFamily={'Ubuntu'}></Txt>
+    </>
+  );
+
+  yield* opacity(100, 1);
+  yield* waitUntil("hideTitle");
 });
